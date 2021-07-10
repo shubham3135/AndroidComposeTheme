4f10a034d91defe9b0d1a4b4616b8724518e0e59
Shubham <shubhamkumarwinner@gmail.com>
2021-07-10 11:48:09 +0530

    Adding dependency

--- app/src/main/java/com/shubhamkumarwinner/composetheme/ui/theme/Theme.kt
+++ app/src/main/java/com/shubhamkumarwinner/composetheme/ui/theme/Theme.kt
@@ -28,7 +28,10 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ComposeThemeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun ComposeThemeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
